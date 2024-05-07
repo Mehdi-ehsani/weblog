c6a28112eb9d1202c874e39f86c123b35612116d
Mahdi Ehsani <metti.work@gmail.com>
2024-05-07 19:57:10 +0330

    add and setup font

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 function App() {
   return (
     <>
-      <h1>start</h1>
+      <h1>سلام</h1>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
+import "./styles/index.css";
+import "./styles/font.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
