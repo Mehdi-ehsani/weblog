9f8627570cd781fd9dd283e2307949b818347b36
Mahdi Ehsani <metti.work@gmail.com>
2024-05-09 12:31:12 +0330

    config .env file and wrap graphQL in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { gql, useQuery } from "@apollo/client"
 
 function App() {
+  const QL = gql`
+    query {
+  authors{
+    name,
+    id,
+    email,
+    slug
+  },
+  posts{
+    title,
+    description{
+      text
+    },
+    cover {
+      url
+    }
+    
+  }
+}
+  `
+  const {loading , data} = useQuery(QL)
+  console.log(data)
   return (
     <>
       <h1 className="text-yellow-700"> ssسلام</h1>
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+//-----css-----//
 import "./styles/index.css";
 import "./styles/font.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const client = new ApolloClient({
+	uri: import.meta.env.VITE_HYGRAPH_URI,
+	cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>
+);
